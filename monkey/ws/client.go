9a29c5f6b4d8466034c85589c734f759c99ef70c
Tom Haines <tom@tomhaines.xyz>
2022-02-26 16:33:13 +0000

    fuck cors

--- monkey/ws/client.go
+++ monkey/ws/client.go
@@ -27,6 +27,8 @@ var (
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+
+	CheckOrigin: func(r *http.Request) bool { return true },
 }
 
 // Client manages a signle websocket connection between the server and a client.
