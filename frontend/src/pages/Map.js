d3c4c0d5b74af1a878ce9fb39d4f7200fa655982
Paolo <paolo@eduroam-102-31.nomadic.bris.ac.uk>
2022-02-26 17:37:53 +0000

    first rudimentary attempt at the map page

--- frontend/src/pages/Map.js
+++ frontend/src/pages/Map.js
@@ -1,4 +1,4 @@
-import { Box, Toolbar, Container, Grid, Paper } from '@mui/material';
+import { Box, Button, ButtonGroup, Checkbox, Container, FormControlLabel, Grid, Paper, Toolbar } from '@mui/material';
 import Title from '../components/Title';
 
 const Map = () => {
@@ -15,10 +15,52 @@ const Map = () => {
         }}>
         <Toolbar />
         <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-            <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column', mt: '0.5rem' }}>
-                <Title>Map</Title>
-                <p>lorem ipsym lorem ipsymlorem ipsymlorem ipsymlorem ipsymlorem ipsym</p>
-            </Paper>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column', mt: '0.5rem' }}>
+                        <Title>Event name</Title>
+                        <Grid container spacing={2}>
+                            <Grid item xs={3}>x/y responded</Grid>
+                            <Grid item xs={3}>x days to go</Grid>
+                            <Grid item xs={3}>
+                                <p>invite link:</p>
+                                <a href='http://localhost:3000/'>Share this URL</a>
+                            </Grid>
+                        </Grid>
+                    </Paper>
+                </Grid>
+                <Grid item xs={2}>
+                    <Paper>
+                        <FormControlLabel control={<Checkbox defaultChecked />} label="Show crawlers" />
+                        <Title>Routes:</Title>
+                        <ButtonGroup orientation='vertical'>
+                            <Button>AI</Button>
+                            <Button>K Means</Button>
+                            <Button>Fosters</Button>
+                            <Button>TSP</Button>
+                        </ButtonGroup>
+                    </Paper>
+                </Grid>
+                <Grid item xs={7}>
+                    <Paper>Map</Paper>
+                </Grid>
+                <Grid item xs={3}>
+                    <Paper>
+                        <Title>K Means Algorithm</Title>
+                        <p>
+                            This algorithm assigns each of the given datapoints to one of k different cluster centres. 
+                            It uses a machine learning approach to update its prediction of the cluster centres as well as the assignment of datapoints to these centres.
+                        </p>
+                        <Title>Stats:</Title>
+                        <Grid container spacing={2}>
+                            <Grid item xs={6}>10 tonnes</Grid>
+                            <Grid item xs={6}>£50</Grid>
+                            <Grid item xs={6}>2km walked</Grid>
+                            <Grid item xs={6}>100 pint options</Grid>
+                        </Grid>
+                    </Paper>
+                </Grid>
+            </Grid>
         </Container>
     </Box>
     )
