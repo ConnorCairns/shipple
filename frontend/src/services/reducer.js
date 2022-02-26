6ba80fd29010eed1db9a0649c7e202a9b476d3e7
Tom Haines <tom@tomhaines.xyz>
2022-02-26 20:53:32 +0000

    Impl create form

--- frontend/src/services/reducer.js
+++ frontend/src/services/reducer.js
@@ -1,7 +1,8 @@
 export const initialState = {
     connected: false,
     date: new Date(),
-    name: "",
+    crawlName: "",
+    userName: "",
     walkingDist: 10,
     crawlID: "",
     algorithm: "ai"
@@ -11,8 +12,10 @@ export const reducer = (state, action) => {
     switch(action.type){
         case 'updateDate':
             return {...state, date: action.payload}
-        case 'updateName':
-            return {...state, name: action.payload}
+        case 'updateUserName':
+            return {...state, userName: action.payload}
+        case 'updateCrawlName':
+            return {...state, crawlName: action.payload}
         case 'connected':
             return {...state, connected: true}
         case 'updateWalkingDist':
