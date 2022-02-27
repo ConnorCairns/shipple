1f1e8acf67857081166a7ff941b92842fc8a7da0
Paolo <paolo@eduroam-102-31.nomadic.bris.ac.uk>
2022-02-27 11:19:56 +0000

    first attempt at route finding

--- route_finder/main.py
+++ route_finder/main.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+from pandas import DataFrame
 from methods import build_map, plot_iso_map, make_intersect, format_data, pub_path
 from data import save_graph, read_graph, df_from_publist, read_center
 import time
 import warnings
 import json
+import numpy as np
 
 warnings.filterwarnings("ignore", category=UserWarning)
 
@@ -34,21 +36,47 @@ def find_bounding_poly(data=None):
     return out
 
 
+def euclidean_distance(coord1, coord2):
+    c1 = np.array(coord1)
+    c2 = np.array(coord2)
+    return np.linalg.norm(c1, c2)
+
+
+def closest_pub(pubs, coordinate):
+    return min([i for i in range(len(pubs))], key=lambda i: euclidean_distance(pubs[i][1:], coordinate))
+
+
 def create_pub_route(publist=None):
+    NUM_PUBS = 5
 
     pubs_df = df_from_publist(publist)
-
     centroid = read_center('temps/temp_center')
-
     pub_path(pubs_df, centroid)
 
+    pubs = pubs_df.values.tolist()
 
+    central_pub_index = closest_pub(pubs, centroid)
+    central_pub = pubs.pop(central_pub_index)
 
+    selected_pubs = [central_pub]
 
+    for _ in range(NUM_PUBS - 1):
+        # find the closest pub to each of the end points
+        start_pub = selected_pubs[0]
+        end_pub = selected_pubs[-1]
 
+        i_s = closest_pub(pubs, start_pub[1:])
+        i_e = closest_pub(pubs, end_pub[1:])
 
+        # choose the closest one of these to add to the selected list
+        if euclidean_distance(start_pub[1:], pubs[i_s][1:]) < euclidean_distance(end_pub[1:], pubs[i_e][1:]):
+            new_pub = pubs.pop(i_s)
+            selected_pubs = [new_pub] + selected_pubs
+        else:
+            new_pub = pubs.pop(i_e)
+            selected_pubs.append(new_pub)
 
-
+    return selected_pubs
 
 
 if __name__ == '__main__':
